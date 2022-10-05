04742ce78d9070aac166a2ae896cb05f051a39f1
Gabriel <Gabriel.nascimento@dcomp.ufs.br>
2022-10-04 23:09:54 -0300

    Disponibilizando api para toda aplicação

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,10 +1,15 @@
 import 'package:api_/app/json_placeholder/json_placeholder_module.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+
+import 'core/api/dio/dio_impl.dart';
 
 class AppModule extends Module {
   @override
   final List<Bind> binds = [
-    // Bind.instance<Dio>(Dio()),
+    //api
+    Bind.lazySingleton((i) => Dio()),
+    Bind.lazySingleton((i) => DioNetworkImpl(i<Dio>())),
   ];
 
   @override
